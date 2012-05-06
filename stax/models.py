b3d7cd109038035e037e849e5a9c15c0e7dc4d5b
Thomas McKee <caffeine.storm@gmail.com>
2012-05-06 14:51:30 -0400

    They almost look like stacks...

--- stax/models.py
+++ stax/models.py
@@ -3,14 +3,14 @@ from django.db import models
 class StackNode( models.Model ):
     name = models.CharField(max_length=256)
     desc = models.TextField()
-    down = models.ForeignKey( 'self', null=True )
+    parent = models.ForeignKey('self', null=True)
 
     def __unicode__(self):
         return self.name
 
 class Stack( models.Model ):
     name = models.CharField(max_length=256)
-    top = models.ForeignKey( StackNode )
+    top = models.ForeignKey( StackNode, null=True )
 
     def __unicode__(self):
         return self.name
--- stax/views.py
+++ stax/views.py
@@ -1,23 +1,22 @@
 from django.shortcuts import render_to_response
 from stax.models import Stack
 
-stylelist = [
-    "dotted",
-    "dashed",
-    "solid",
-    "groove",
-    "ridge",
-    "inset",
-    "outset",
-    "double",
-]
+def get_stax():
+    allStax = Stack.objects.all()
+    ret = {}
+    for st in allStax:
+        ret[str(st.name)] = []
+        top = st.top
+        while top:
+            ret[str(st.name)].append( str(top.name) )
+            top = top.parent
+    return ret
 
 def frontpage(req):
     return render_to_response(
         'stax/index.html',
         {
-            'all_stax' : Stack.objects.all(),
-            'teststyles' : stylelist
+            'all_stax' : get_stax()
         }
     )
 
