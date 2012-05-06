2273998768e5dd3385a39829f0fe33d0b8fb43fd
Thomas McKee <caffeine.storm@gmail.com>
2012-05-06 11:58:06 -0400

    Rounded corners for a stack base

--- stax/views.py
+++ stax/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render_to_response
 from stax.models import Stack
 
+stylelist = [
+    "dotted",
+    "dashed",
+    "solid",
+    "groove",
+    "ridge",
+    "inset",
+    "outset",
+    "double",
+]
+
 def frontpage(req):
-    return render_to_response( 'stax/index.html', { 'all_stax' : Stack.objects.all() } )
+    return render_to_response(
+        'stax/index.html',
+        {
+            'all_stax' : Stack.objects.all(),
+            'teststyles' : stylelist
+        }
+    )
 
