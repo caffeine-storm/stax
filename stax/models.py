aebfde2dbce6f62882ef40fc9ec8a8d51d818c14
Thomas McKee <tmckee@ceeqit.com>
2012-07-18 11:21:49 -0400

    V2 model

--- stax/models.py
+++ stax/models.py
@@ -1,25 +1,29 @@
 from django.db import models
 
+class NodeType( models.Model ):
+    NODE_TYPE_CHOICES = (
+        ( 0, 'Invalid' ),
+        ( 1, 'Standard' ),
+        ( 2, 'Base' ),
+        # ( 3, 'Blocker' ),
+    )
+
+    desc = models.SmallIntegerField( choices=NODE_TYPE_CHOICES, primary_key=True, default=1 )
+
+    def __unicode__(self):
+        return NODE_TYPE_CHOICES[self.desc][1]
+
 class StackNode( models.Model ):
     name = models.CharField(max_length=256)
     desc = models.TextField()
-    parent = models.ForeignKey('self', null=True)
+    tp = models.ForeignKey( NodeType, db_index=True )
 
     def __unicode__(self):
         return self.name
 
-class Stack( models.Model ):
-    name = models.CharField(max_length=256)
-    top = models.ForeignKey( StackNode, null=True )
+class Dependency( models.Model ):
+    parent = models.ForeignKey( StackNode, primary_key=True, db_index=True, related_name='parent' )
+    child = models.ForeignKey( StackNode, db_index=True, related_name='child' )
 
     def __unicode__(self):
-        return self.name
-
-    def doPop(self):
-        if not self.top:
-            return None
-        ret = self.top
-        self.top = ret.parent
-        ret.parent = None
-        return ret
-
+        return self.child.name + " depends on " + self.parent.name
